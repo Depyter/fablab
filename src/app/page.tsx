d78169c32ee3c544fbd6616edf65c5ec3ea1a6fa
Depyter <acabalharleyvan@gmail.com>
2026-02-19 17:15:00 +0800

    added file upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { ComponentExample } from "@/components/component-example";
+import { FileUploadExample } from "@/components/file-upload-example";
 
 export default function Page() {
-  return <ComponentExample />;
+  return (
+    <>
+      <ComponentExample />
+    </>
+  );
 }
