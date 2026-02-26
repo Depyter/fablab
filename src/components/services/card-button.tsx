e574f1f5fe3bcb62a06b97afa665eafb34884bba
Depyter <acabalharleyvan@gmail.com>
2026-02-26 15:08:48 +0800

    create CI and formatted the files

--- src/components/services/card-button.tsx
+++ src/components/services/card-button.tsx
@@ -1,5 +1,5 @@
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardAction,
@@ -7,19 +7,22 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
-import { CirclePlus } from "lucide-react"
-
-
+import { CirclePlus } from "lucide-react";
 
 export function CardButton() {
   return (
-    <Card className="bg-transparent relative mx-auto w-full max-w-sm pt-0 border border-primary border-2 hover:bg-primary-muted items-center justify-center" onClick={() => alert("Card button clicked!")}>
+    <Card
+      className="bg-transparent relative mx-auto w-full max-w-sm pt-0 border border-primary border-2 hover:bg-primary-muted items-center justify-center"
+      onClick={() => alert("Card button clicked!")}
+    >
       <div className="flex flex-col items-center justify-center p-4">
         <CirclePlus className="size-12 text-primary" />
-        <p className="mt-2 text-lg font-semibold text-primary">Add New Service</p>
+        <p className="mt-2 text-lg font-semibold text-primary">
+          Add New Service
+        </p>
       </div>
     </Card>
-  )
+  );
 }
--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -99,11 +99,15 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props} className="bg-transparent hover:bg-primary-muted">
+    <Sidebar
+      collapsible="icon"
+      {...props}
+      className="bg-transparent hover:bg-primary-muted"
+    >
       <SidebarHeader>
         <TeamSwitcher teams={data.teams} />
       </SidebarHeader>
-      <SidebarContent >
+      <SidebarContent>
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter>
