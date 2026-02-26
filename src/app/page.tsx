9ac75eaf19bcce60e40abd4d15cf1c6b1683ca7e
Depyter <acabalharleyvan@gmail.com>
2026-02-26 16:08:08 +0800

    ran migration to opennext for cloudflare workers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { ComponentExample } from "@/components/component-example";
-import { FileUploadExample } from "@/components/file-upload-example";
 
 export default function Page() {
   return (
