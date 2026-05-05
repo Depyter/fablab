b87e2f7bfcb1c69002ba1bb332e4babc9fdd67e0
Depyter <acabalharleyvan@gmail.com>
2026-05-05 22:07:51 +0800

    Revert "ensure contents is in the safe area for mobile browsers."
    This reverts commit 8e75ae5efc87167fe8c2ae0ada7b694147c7e294.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
@@ -9,10 +9,6 @@ const robotoMono = Roboto_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 });
-
-export const viewport: Viewport = {
-  viewportFit: "cover",
-};
 
 export const metadata: Metadata = {
   title: "IskoLab | Fablab UP Cebu",
