4a7f7d26ff04ee3a833b8ed1b1de9752b4e22011
Depyter <acabalharleyvan@gmail.com>
2026-05-07 15:03:00 +0800

    add webmanifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,27 @@ const robotoMono = Roboto_Mono({
 
 export const metadata: Metadata = {
   title: "IskoLab | Fablab UP Cebu",
-  description: "Make almost Everying!",
+  description: "Make almost Everything!",
+  manifest: "/site.webmanifest",
   icons: {
-    icon: "/fablab.svg",
+    icon: [
+      { url: "/fablab.svg" },
+      { url: "/ios/16.png", sizes: "16x16", type: "image/png" },
+      { url: "/ios/32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/ios/57.png", sizes: "57x57", type: "image/png" },
+      { url: "/ios/60.png", sizes: "60x60", type: "image/png" },
+      { url: "/ios/72.png", sizes: "72x72", type: "image/png" },
+      { url: "/ios/76.png", sizes: "76x76", type: "image/png" },
+      { url: "/ios/114.png", sizes: "114x114", type: "image/png" },
+      { url: "/ios/120.png", sizes: "120x120", type: "image/png" },
+      { url: "/ios/144.png", sizes: "144x144", type: "image/png" },
+      { url: "/ios/152.png", sizes: "152x152", type: "image/png" },
+      { url: "/ios/167.png", sizes: "167x167", type: "image/png" },
+      { url: "/ios/180.png", sizes: "180x180", type: "image/png" },
+      { url: "/ios/1024.png", sizes: "1024x1024", type: "image/png" },
+    ],
   },
 };
 
